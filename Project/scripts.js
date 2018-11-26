7acfd219161911b62d42c1b248a1a48bfb5de116
SteveLeong <steveleong8@gmail.com>
2018-11-25 23:55:43 -0700

    matrixify

--- Project/scripts.js
+++ Project/scripts.js
@@ -3,123 +3,79 @@ const canvas = document.getElementById("photo");
 const ctx = canvas.getContext("2d");
 const strip = document.getElementById("strip");
 
-ctx.font="12px monospace";
-ctx.fillStyle="#00BB00";
+ctx.font = "12px monospace";
+ctx.fillStyle = "#00BB00";
+var fontSize = 12;
 var symbols = new Array(20);
+var symbolData = [];
+
+var streams = [];
 
 
 
 
+function setup() {
 
-function setup(){
-    
     getSymbols();
-    
+
     getVideo();
-    
-}
 
 
-
-
-function getSymbols(){
-
-    var symbolData = new Array();
-
-    // Get Katakana symbols
-    var xcoor = 0;
-    var count = 0;
-    var ycoor = 10;
-
-    for(var i = 0; i < 96; i++){
-        
-        // get symbol, 0x30A0 is where Katakana starts
-        symbol = String.fromCharCode(0x30A0 + i);
-
-        if(((count*12) + 20) > canvas.width) {
-            count = 0;
-        }
-
-        if(count == 0){
-            ycoor += 10;
-        }
-
-        xcoor = (count*12) + 20;
-        
-        ctx.fillText(symbol,xcoor,ycoor)
-        
-        // get the 'brightness' of the symbol, Black=000, so brightness is the amount of RGB in the symbol
-        // getImageData(x, y, width, height)
-        
-        var imgData=ctx.getImageData(xcoor, ycoor-10, 7, 11);
-
-        // total is the total RGB value of the symbol
-        var total = 0; //reset
-        for (var j = 0; j < imgData.data.length; j += 4){
-            total += imgData.data[j] + imgData.data[j + 1] + imgData.data[j + 2];
-        }
-
-        var arr = new Array(2);
-        arr[0] = total;
-        arr[1] = symbol;
-        //console.log(arr)
-        symbolData.push(arr);
-        count++;
+    var x = 0;
+    for (var i = 0; i <= canvas.width / fontSize; i++) {
+      var stream = new Stream();
+      stream.generateSymbols(x, (Math.random() - 1000));
+      streams.push(stream);
+      x += fontSize;
     }
-    //console.log(symbolData);
-
-    // Sort symbols by total values
-
-    symbolData.sort(function(a,b) {return a[0] - b[0];});
-    
-    //console.log(symbolData);
-    // print for debug purposes
-    for(var i = 0; i < symbolData.length; i++)
-    {
-        ctx.fillText(symbolData[i][1],(i*10 )+ 20,50);
-    }
-
-    // Take 20 symbols
-
-    var interval = Math.floor(symbolData.length/symbols.length);
-    //console.log(symbolData.length, interval);
-
-    var index = 0;
-    for(var i = 0; i < symbols.length-1 && index < symbolData.length; i++ )
-    {
-        symbols[i] = symbolData[index][1];
-        ctx.fillText(symbols[i],(i*10 )+ 20,60);
-        index+= interval;
-    }
-
-    symbols[symbols.length-1] = symbolData[symbolData.length-1][1];
-    console.log(symbols);
-    //pixels.reverse();
-    ctx.fillText(symbols[symbols.length-1],15*6,80);
 
 }
 
 
+function Symbol(x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.value
+
+    this.setRandomSymbol = function(){
+        var rand = Math.floor(Math.random() * 95);
+        this.value = symbolData[rand][1];
+    }
+
+    this.animate = function(){
+        this.y = (this.y >= canvas.height) ? 0 : this.y += this.speed;
+    }
+}
 
 
-function getVideo(){
+function Stream() {
+    this.symbols = [];
+    this.totalSymbols = Math.round((Math.random() * 35) + 5);
+    this.speed = (Math.random()* 5) + 1;
 
-    navigator.mediaDevices.getUserMedia({video: true, audio: false})
-        .then (localMediaStream => {
-            console.log(localMediaStream)
-            video.src = window.URL.createObjectURL(localMediaStream);
-            video.play();
-        })
-        .catch(err => {
-            console.error("Please enable your webcam", err);
+    this.generateSymbols = function(x, y) {
+        for (var i =0; i <= this.totalSymbols; i++) {
+            symbol = new Symbol(x, y, this.speed);
+            symbol.setRandomSymbol();
+            this.symbols.push(symbol);
+            y -= fontSize;
+        }
+    }
+
+    this.render = function() {
+        this.symbols.forEach(function(symbol) {
+            ctx.fillStyle = "#00BB00";
+            ctx.fillText(symbol.value, symbol.x, symbol.y);
+            symbol.animate();
+            //symbol.setRandomSymbol();
         });
-
+    }
 }
 
 
 
-
-function paintToCanvas(){
+function paintToCanvas() {
 
     // const width = video.videoWidth;
     // const height = video.videoHeight;
@@ -127,9 +83,8 @@ function paintToCanvas(){
     // canvas.width = width;
     // canvas.height = height;
 
-    ctx.drawImage(video, 0, 0);
-
-    matrixFilter();
+    matrixify();
+    // matrixFilter();
     //let pixels = ctx.getImageData(0, 0, width, height);
 
     //pixels = matrixify(pixels)
@@ -142,29 +97,86 @@ function paintToCanvas(){
 
 
 
-function matrixFilter() {
-    var fontsize = 12;
-    var blockWidth = fontsize - 2;
-    var blockHeight = fontsize - 1;
+function getSymbols() {
 
-    rows = canvas.height/blockHeight;
-    cols = canvas.width/blockWidth;
+    // Get Katakana symbols
+    var xcoor = 0;
+    var count = 0;
+    var ycoor = 10;
 
-    for(var i=0; i < rows; i++){
-        for(var j = 0; j < cols; j++) {
-            var imgData=ctx.getImageData(j*blockWidth,i*blockHeight,blockWidth,blockHeight);
-            var brightness = getBrightness(imgData);
+    for (var i = 0; i < 96; i++) {
 
-            total = brightness[0] / brightness[1];
-            
-            var average = Math.floor(total / symbols.length);
-            //console.log(average);
-            ctx.clearRect(j*blockWidth,i*blockHeight, blockWidth, blockHeight);	
+        // get symbol, 0x30A0 is where Katakana starts
+        symbol = String.fromCharCode(0x30A0 + i);
 
-            ctx.fillText(symbols[average],j*blockWidth,(i+1)*blockHeight)
-            
+        if (((count * 12) + 20) > canvas.width) {
+            count = 0;
         }
+
+        if (count == 0) {
+            ycoor += 10;
+        }
+
+        xcoor = (count * 12) + 20;
+
+        ctx.fillText(symbol, xcoor, ycoor)
+
+        // get the 'brightness' of the symbol, Black=000, so brightness is the amount of RGB in the symbol
+        // getImageData(x, y, width, height)
+
+        var imgData = ctx.getImageData(xcoor, ycoor - 10, 7, 11);
+
+        // total is the total RGB value of the symbol
+        var total = 0; //reset
+        for (var j = 0; j < imgData.data.length; j += 4) {
+            total += imgData.data[j] + imgData.data[j + 1] + imgData.data[j + 2];
+        }
+
+        var arr = new Array(2);
+        arr[0] = total;
+        arr[1] = symbol;
+        //console.log(arr)
+        symbolData.push(arr);
+        count++;
     }
+
+    // Sort symbols by total values
+    symbolData.sort(function (a, b) { return a[0] - b[0]; });
+
+
+    xcoor = 0;
+    ycoor = 50;
+    count = 0;
+    // print for debug purposes
+    for (var i = 0; i < symbolData.length; i++) {
+        if (((count * 12) + 20) > canvas.width) {
+            count = 0;
+        }
+
+        if (count == 0) {
+            ycoor += 10;
+        }
+
+        xcoor = (count * 12) + 20;
+        ctx.fillText(symbolData[i][1],xcoor, ycoor);
+
+        count++;
+    }
+
+    // Take 20 symbols
+    var interval = Math.floor(symbolData.length / symbols.length);
+    var index = 0;
+    for (var i = 0; i < symbols.length - 1 && index < symbolData.length; i++) {
+        symbols[i] = symbolData[index][1];
+        ctx.fillText(symbols[i], (i * 10) + 20, 90);
+        index += interval;
+    }
+
+    symbols[symbols.length - 1] = symbolData[symbolData.length - 1][1];
+    console.log(symbols);
+    //pixels.reverse();
+    ctx.fillText(symbols[symbols.length - 1], 15 * 6, 110);
+
 }
 
 
@@ -173,13 +185,79 @@ function matrixFilter() {
 function getBrightness(imgData) {
     var count = 1;
     var total = 0;
-    for (var k=0; k < imgData.data.length; k+=4)
-    {
-        total += ((0.2126*imgData.data[k]) + (0.7152*imgData.data[k+1]) + (0.0722*imgData.data[k+2]));
+    for (var k = 0; k < imgData.data.length; k += 4) {
+        total += ((0.2126 * imgData.data[k]) + (0.7152 * imgData.data[k + 1]) + (0.0722 * imgData.data[k + 2]));
         count++;
     }
 
     return [total, count]
+}
+
+
+
+
+function matrixify() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    streams.forEach(function(stream) {
+        stream.render();
+    });
+}
+
+
+
+
+function matrixFilter() {
+
+    ctx.drawImage(video, 0, 0);
+    var blockWidth = fontSize - 2;
+    var blockHeight = fontSize - 1;
+
+    rows = canvas.height / blockHeight;
+    cols = canvas.width / blockWidth;
+
+    for (var i = 0; i < rows; i++) {
+        for (var j = 0; j < cols; j++) {
+            var imgData = ctx.getImageData(j * blockWidth, i * blockHeight, blockWidth, blockHeight);
+            var brightness = getBrightness(imgData);
+
+            total = brightness[0] / brightness[1];
+
+            var average = Math.floor(total / symbols.length);
+            //console.log(average);
+            ctx.clearRect(j * blockWidth, i * blockHeight, blockWidth, blockHeight);
+
+            ctx.fillText(symbols[average], j * blockWidth, (i + 1) * blockHeight)
+
+        }
+    }
+}
+
+
+
+
+function getVideo() {
+
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then(localMediaStream => {
+            console.log(localMediaStream)
+            video.src = window.URL.createObjectURL(localMediaStream);
+            video.play();
+        })
+        .catch(err => {
+            console.error("Please enable your webcam", err);
+        });
+
+}
+
+function takePhoto() {
+    const data1 = canvas.toDataURL("image/jpeg");
+    console.log(data1);
+    var link = document.createElement("a");
+    link.href = data1;
+    link.setAttribute("download", "matrix");
+    link.innerHTML = `<img src="${data1}"/>`;
+    strip.insertBefore(link, strip.firstChild);
 }
 
 setup();
